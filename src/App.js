87b2313bd65879b7b0104ab670c032b339d64da6
Casper Yonel <yonelcasper@gmail.com>
2021-12-01 11:23:02 -0700

    router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div className="App">
-       <h1>Yo</h1>
-    </div>
+    <>
+      <Router>
+         <Navbar />
+         <Routes>
+           <Route path='/' exact />
+         </Routes>
+      </Router>
+    </>
   );
 }
 
