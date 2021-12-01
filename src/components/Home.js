ca90df3b1e69d2a63b4d7099117f35a6657f64c6
Casper Yonel <yonelcasper@gmail.com>
2021-12-01 15:36:02 -0700

    projects and home tabs completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import profilePhoto from "../profilePhoto2.png"
 
 const Home = () => {
     
@@ -7,9 +8,11 @@ const Home = () => {
     return (
         <div className="Home">
             <div className="Home-Container">
+                
+                <img src={profilePhoto} alt="" id="profile-photo" />
                 <div id="name-title">Casper Yonel</div>
                 <p className='p-text'>Hello! Welcome to my website.</p>
-                <p className='p-text'>I'm a passionate 23 year old looking to start companies and build products that better the world.</p>
+                <p className='p-text long-line'>I'm a passionate 23 year old looking to start companies and build products that better and move the world forward.</p>
                 <p className='p-text long-line'>I studied finance at Penn State University, and have worked at J.P. Morgan, Citi, and Point72 Asset Management.</p>
                 <p className='p-text' >If you'd like to get in contact, please click {here}</p>
             </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import YoutubeEmbed from "./video/YoutubeEmbed";
+
 
 const Projects = () => {
+    
+    const here = <a href="https://github.com/casperyonel/landing-page-salaries-2" target="_blank">here</a>
+
+    
     return (
         <div>
-            <h2 className="this">Projects</h2>
-            <h2 className="this">Projects</h2>
-            <h2 className="this">Projects</h2>
-            <h2 className="this">Projects</h2>
-            <h2 className="this">Projects</h2>
-            <h2 className="this">Projects</h2>
-            <h2 className="this">Projects</h2>
-            <h2 className="this">Projects</h2>
-            <h2 className="this">Projects</h2>
+            <p className='p-text long-line Subtitle'>Project: Solidity Landing Page</p>
+            <p className='p-text long-line'>I built this website for a company I am looking to build in the near future. The company is a capital generative marketplace that plugs into the backend of employer payroll systems. Our job is to collect investor capital and provide it to employers to pay out salaries to their employees in advance. This way employees of the company can have the flexibility of accessing their entire future salary early. I suspect this to be most used on a 1 or 2 month basis. How much eaiser would the world be if we could get paid just a bit more each paycheck to make paying rent a little bit easier!</p>
+            <YoutubeEmbed embedId="EVAz3OATmu8" />
+            <ul>
+                <p className="Subtitle">Technologies used:</p>
+                <li className="list">Javascript</li>
+                <li className="list">HTML/CSS</li>
+                <li className="list">React</li>
+                <li className="list">NodeJS</li>
+                <li className="list">Express</li>
+                <li className="list">Axios</li>
+                <li className="list">Heroku</li>
+                <li className="list">PostgreSQL</li>
+            </ul>
+            <p>Click {here} to check out the github repo for the project.</p>
         </div>
     )
 }
